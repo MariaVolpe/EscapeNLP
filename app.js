fa7c49e125833d35a4a200c13b6cd4a1722dce5f
Ishmaelk <kheir1@msn.com>
2019-02-28 09:16:07 -0500

    whoops forgot to include package.json and app.js, removed nlp testing from app.js to make things cleaner

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// glove (John told me about this) if training doesnt work well
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+
+app.listen(port, () => console.log (`EscapeNLP Server listening on port ${port}!`));
