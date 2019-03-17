7c17f9f3315ed6659df0b83f0d3d94703c967302
Maria Volpe <MariaCHVolpe@gmail.com>
2019-03-16 22:07:37 -0400

    Restore app.js (#22)
    * Restore app.js
    
    * Fix typo on FE
    
    * Edit to app.js
    
    * Change port on BE

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => {
+  console.log(`EscapeNLP Server listening on port ${port}!`); // eslint-disable-line no-console
+});
