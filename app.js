92fe665bad51fdae38097f42d78010a24386c325
Maria Volpe <mariaCHVolpe@gmail.com>
2019-03-23 13:05:30 -0400

    Set up skeletal game session controllers

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const helmet = require('helmet');
+const gameSessionRouter = require('./routers/GameSession');
+
 
 const app = express();
-const port = 8000;
+const PORT = 8000;
+
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.use('/game', gameSessionRouter);
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => {
-  console.log(`EscapeNLP Server listening on port ${port}!`); // eslint-disable-line no-console
+app.listen(PORT, () => {
+  console.log(`EscapeNLP Server listening on port ${PORT}!`); // eslint-disable-line no-console
 });
+
