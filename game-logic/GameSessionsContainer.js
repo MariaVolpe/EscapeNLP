78b7a2eef1aa64152ce79036f5c1c09cfc24d2aa
Maria Volpe <mariaCHVolpe@gmail.com>
2019-03-24 15:41:48 -0400

    Finish GameSession controllers

--- game-logic/GameSessionsContainer.js
+++ game-logic/GameSessionsContainer.js
@@ -5,7 +5,8 @@ const GameSession = require('./GameSession.js');
 class GameSessionsContainer {
   constructor() {
     this.games = new Map();
-    this.gameCount = 0;
+    this.gameIdCounter = 0;
+    this.playerIdCounter = 0;
   }
 
   // rename b/c there's nothing about this that would indicate
@@ -16,12 +17,40 @@ class GameSessionsContainer {
     return this.gameCount++;
   }
 
-  removeGame(id) {
+  removeGame(id, callback) {
     if (!this.games.has(id)) {
       // todo: do we need to do anything here?
-      return;
+      return callback('requested gameId doesnt exist');
     }
     this.games.delete(id);
+    return callback(null);
+  }
+
+  startGame(id, callback) {
+    if (!this.games.has(id)) {
+      // todo: do we need to do anything here?
+      return callback('requested gameId doesnt exist');
+    }
+    this.games.get(id).generateGame();
+    return callback(null);
+  }
+
+  addPlayerToGameSession(gameId, loggedInPlayerId, callback) {
+    if (!this.games.has(gameId)) {
+      return callback('requested gameId doesnt exist');
+    }
+    const playerId = loggedInPlayerId || this.playerIdCounter;
+    this.games.get(gameId).addPlayerToGameSession(playerId);
+    this.playerIdCounter++;
+    return callback(null, playerId);
+  }
+
+  dropPlayerFromSession(gameId, playerId, callback) {
+    if (!this.games.has(gameId)) {
+      return callback('requested gameId doesnt exist');
+    }
+    this.games.get(gameId).dropPlayerFromSession(playerId);
+    return callback(null);
   }
 }
 
--- routers/GameSession.js
+++ routers/GameSession.js
@@ -1,37 +1,72 @@
 const express = require('express');
-const generateId = require('../helpers/generateId');
+const GameSessionsContainer = require('../game-logic/GameSessionsContainer');
 
 const router = express.Router();
+const GameContainer = new GameSessionsContainer();
 
 const parseAction = (req, res, next) => {
   // NLP parse
   next();
 };
 
+const fail = (req, res) => {
+  if (!res.headersSent) {
+    res.sendStatus(500);
+  }
+};
+
 router.get('/:gameId', (req, res) => {
-  // fetch game updates ?
+  // todo: fetch game updates ?
   res.json({ place: 'holder' });
 });
 
 router.post('/', (req, res) => {
-  const gameId = generateId('game');
+  const gameId = GameContainer.addGame();
   res.json({ gameId });
 });
 
 router.delete('/:gameId', (req, res) => {
+  GameContainer.removeGame(req.params.gameId, (err) => {
+    if (err) {
+      // todo
+      res.sendStatus(404);
+    }
+  });
   res.sendStatus(204);
 });
 
 router.post('/:gameId/player', (req, res) => {
+  let playerId = null;
   // todo: check if user is logged in
   // if so grab from database
   // else for guests...:
-  const playerId = generateId('player');
+  GameContainer.addPlayerToSession(req.params.gameId, playerId, (err, id) => {
+    if (err) {
+      // todo
+      res.sendStatus(404);
+    }
+    playerId = id;
+  });
   res.json({ playerId });
 });
 
 router.delete('/:gameId/player/:playerId', (req, res) => {
-  // todo
+  GameContainer.dropPlayerFromSession(req.params.gameId, req.params.playerId, (err) => {
+    if (err) {
+      // todo
+      res.sendStatus(404);
+    }
+  });
+  res.sendStatus(204);
+});
+
+router.post('/:gameId/start', (req, res) => {
+  GameContainer.startGame(req.params.gameId, (err) => {
+    if (err) {
+      // todo
+      res.sendStatus(404);
+    }
+  });
   res.sendStatus(204);
 });
 
