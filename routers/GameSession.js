4ab77545bad4416f1ced9352187cb5f446ded32f
Maria Volpe <mariaCHVolpe@gmail.com>
2019-03-24 00:39:21 -0400

    Add route for posting game action

--- routers/GameSession.js
+++ routers/GameSession.js
@@ -1,7 +1,12 @@
 const express = require('express');
-const generateId = require('./helpers/generateId');
+const generateId = require('../helpers/generateId');
 
 const router = express.Router();
+
+const parseAction = (req, res, next) => {
+  // NLP parse
+  next();
+};
 
 router.get('/:gameId', (req, res) => {
   // fetch game updates ?
@@ -21,13 +26,17 @@ router.post('/:gameId/player', (req, res) => {
   // todo: check if user is logged in
   // if so grab from database
   // else for guests...:
-  const playerId = generateAgentId('player'); // todo: generate player id
+  const playerId = generateId('player');
   res.json({ playerId });
 });
 
 router.delete('/:gameId/player/:playerId', (req, res) => {
   // todo
   res.sendStatus(204);
+});
+
+router.post('/:gameId/action', parseAction, (req, res) => {
+  res.json({ actionResult: 'text' });
 });
 
 module.exports = router;
