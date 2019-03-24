2a2c38d979186c1fc09e9261ec8b9745c04f158f
John Genere <johncgenere@gmail.com>
2019-03-24 02:38:02 -0400

    added how to page

--- client/src/components/HowTo.js
+++ client/src/components/HowTo.js
@@ -11,26 +11,22 @@ class HowTo extends React.Component {
           <br /><br />
           <h2>What is EscapeNLP?</h2>
           <div class="ui divider" style={{marginLeft: '25%', marginRight: '25%'}}></div>
-          <p>
-            Get content from team later
+          <p style={{marginLeft: '25%', marginRight: '25%'}}>
+            EscapeNLP is a freeform multiplayer text-based escape room web game. Players (at most 5) 
+            are placed in a virtual environment where they must collaborate to solve puzzles through 
+            written intentions. Intentions are processed and classified by a natural language 
+            interpreter which then alters the state of the game. With the usage of natural language 
+            processing (or NLP), players will be able to focus on the game, rather than memorizing commands.
           </p>
           <h2>Who can we play EscapeNLP with?</h2>
           <div class="ui divider" style={{marginLeft: '25%', marginRight: '25%'}}></div>
-          <p>
-            Get content from team later
+          <p style={{marginLeft: '25%', marginRight: '25%'}}>
+            You can either play alone or in a team of up to 5 players.
           </p>
           <h2>Rules</h2>
           <div class="ui divider" style={{marginLeft: '25%', marginRight: '25%'}}></div>
           <p>
-            Get content from team later
-          </p>
-          <h3>Solo</h3>
-          <p>
-            Get content from team later
-          </p>
-          <h3>Group</h3>
-          <p>
-            Get content from team later
+            
           </p>
 
 
