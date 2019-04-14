3c2c54f4c6d014c3db1cd7008558fc6dedac1d8c
Nicky Cen <nickycen@hotmail.com>
2019-04-14 13:53:37 -0400

    removed socket from app.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import socketIOClient from 'socket.io-client';
 import Home from './Home';
 import Play from './Play';
 import Browser from './Browser';
@@ -8,8 +7,6 @@ import HowTo from './HowTo';
 import '../styles/App.css';
 
 class App extends Component {
-
-
   render() {
     return (
       <div>
