98d1745afe1859c70bbc7616657bfa26fde384a4
John Genere <johncgenere@gmail.com>
2019-04-22 20:14:25 -0400

    moved more inline styling to stylesheets, except one

--- client/src/components/HowTo.js
+++ client/src/components/HowTo.js
@@ -9,7 +9,7 @@ class HowTo extends React.Component {
         <Navigation inGame={false}/>
           <h1 className="title">How to Play EscapeNLP</h1>
           <h2 className="title">What is EscapeNLP?</h2>
-          <div className="ui divider" style={{marginLeft: '25%', marginRight: '25%'}}></div>
+          <div className="ui divider"></div>
           <p>
             EscapeNLP is a freeform multiplayer text-based escape room web game. Players (at most 5)
             are placed in a virtual environment where they must collaborate to solve puzzles through
@@ -18,12 +18,12 @@ class HowTo extends React.Component {
             processing (or NLP), players will be able to focus on the game, rather than memorizing commands.
           </p>
           <h2>Who can we play EscapeNLP with?</h2>
-          <div className="ui divider" style={{marginLeft: '25%', marginRight: '25%'}}></div>
+          <div className="ui divider"></div>
           <p>
             You can either play alone or in a team of up to 5 players.
           </p>
           <h2>Rules</h2>
-          <div className="ui divider" style={{marginLeft: '25%', marginRight: '25%'}}></div>
+          <div className="ui divider"></div>
           <p>
 
           </p>
