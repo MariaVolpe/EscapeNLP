b8a76a2b1c60fde45f2eb743b1e1d321207a6beb
Ishmaelk <kheir1@msn.com>
2019-05-01 19:17:06 -0400

    edited tests

--- __tests__/game-logic/Grid.js
+++ __tests__/game-logic/Grid.js
@@ -43,10 +43,10 @@ describe('Grid functionality tests', () => {
       // place a moving object and destination at these points //
       removingGrid.add(mover, { x: 0, y: 2 });
       removingGrid.add(destination, { x: 2, y: 0 });
-      removingGrid.removeFromBoard({ obj: mover });
-      removingGrid.removeFromBoard({ name: 'destination' }); // deleting by name
+      removingGrid.removeFromBoard(mover);
+      removingGrid.removeFromBoard(destination); // deleting by name
       removingGrid.add(destination, { x: 2, y: 0 });
-      removingGrid.removeFromBoard({ id: '1' });
+      removingGrid.removeFromBoard(destination);
       expected = JSON.stringify(
         [
           [[wall], [floor], [floor]],
