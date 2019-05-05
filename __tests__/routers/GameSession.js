50d05e17743863791e82b484e49ffc97ea5fa70c
Maria Volpe <mariaCHVolpe@gmail.com>
2019-05-05 17:36:35 -0400

    Remove unused routers

--- __tests__/routers/GameSession.js
+++ __tests__/routers/GameSession.js
@@ -18,23 +18,6 @@ describe('Game Session Router', () => {
     });
   });
 
-  describe('DELETE /game', () => {
-    it('should handle not found', async () => {
-      const response = await request(app).delete('/game/5');
-
-      expect(response.statusCode).toBe(404);
-      expect(response.body.error).toBeDefined();
-      expect(response.body.data).toBeNull();
-    });
-
-    it('should delete a game by ID', async () => {
-      const response = await request(app).delete('/game/1');
-
-      expect(response.statusCode).toBe(204);
-      expect(response.body).toEqual({});
-    });
-  });
-
   describe('POST /game/:gameId/player', () => {
     it('should handle not found', async () => {
       const response = await request(app).post('/game/5/player');
@@ -56,67 +39,6 @@ describe('Game Session Router', () => {
 
       expect(response.statusCode).toBe(201);
       expect(response.body.data).toEqual({ playerId: 1 });
-    });
-  });
-
-  describe('GET /game', () => {
-    it('should return all games in session and their player counts', async () => {
-      const response = await request(app).get('/game');
-
-      const expected = [{
-        gameId: 0,
-        playerCount: 2,
-      }];
-
-      expect(response.statusCode).toBe(200);
-      expect(response.body.data).toEqual(expected);
-    });
-  });
-
-  describe('GET /game/:gameId', () => {
-    it('should handle not found', async () => {
-      const response = await request(app).get('/game/5');
-
-      expect(response.statusCode).toBe(404);
-      expect(response.body.error).toBeDefined();
-      expect(response.body.data).toBeNull();
-    });
-
-    it('should return game', async () => {
-      const response = await request(app).get('/game/0');
-
-      const { data } = response.body;
-
-      expect(response.statusCode).toBe(200);
-      expect(data.id).toEqual(0);
-      expect(data.grid).toBeDefined();
-      expect(data.players).toBeInstanceOf(Array);
-      expect(data.players.length).toEqual(2);
-    });
-  });
-
-  describe('DELETE /game/:gameId/player/:playerId', () => {
-    it('should handle session not found', async () => {
-      const response = await request(app).delete('/game/5/player/1');
-
-      expect(response.statusCode).toBe(404);
-      expect(response.body.error.source).toEqual('gameId');
-      expect(response.body.data).toBeNull();
-    });
-
-    it('should handle player not found', async () => {
-      const response = await request(app).delete('/game/0/player/5');
-
-      expect(response.statusCode).toBe(404);
-      expect(response.body.error.source).toEqual('playerId');
-      expect(response.body.data).toBeNull();
-    });
-
-    it('should delete a player from a session', async () => {
-      const response = await request(app).delete('/game/0/player/1');
-
-      expect(response.statusCode).toBe(204);
-      expect(response.body).toEqual({});
     });
   });
 });
