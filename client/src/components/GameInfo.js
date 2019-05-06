c785ba3d16d0fa676c73162937779f24e6912720
Nicky Cen <nickycen@hotmail.com>
2019-05-05 23:18:07 -0400

    forgot to make minutes only be less than 60

--- client/src/components/GameInfo.js
+++ client/src/components/GameInfo.js
@@ -68,6 +68,7 @@ class GameInfo extends Component {
     let minutes = Math.floor(seconds / 60);
     let hours = Math.floor(minutes / 60);
     seconds = seconds % 60;
+    minutes = minutes % 60;
     let timerSeconds = '';
     let timerMinutes = '';
     let timerHours = '';
