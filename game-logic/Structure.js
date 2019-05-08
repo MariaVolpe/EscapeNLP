7a8500fb559a613a33962b6c11e939d7dd05f177
bricam <brian_campbell@outlook.com>
2019-05-08 16:22:44 -0400

    Moved Flavor Text to separate file

--- game-logic/Structure.js
+++ game-logic/Structure.js
@@ -1,5 +1,6 @@
 const BoardObject = require('./BoardObject');
 const StructLib = require('./board-object-library/structure-library');
+const StructText = require('./board-object-library/structure-text');
 
 /*
  * Class for BoardObjects that are part of a puzzle.
@@ -42,35 +43,35 @@ class Structure extends BoardObject {
     this.activated = false;
 
     // this is the least efficient code i've ever written. sorry to my future self
-    this.moveFalseText = StructLib[name].moveFalseText;
-    this.moveTrueText = StructLib[name].moveTrueText;
-    this.takeFalseText = StructLib[name].takeFalseText;
-    this.takeTrueText = StructLib[name].takeTrueText;
-    this.destroyFalseText = StructLib[name].destroyFalseText;
-    this.destroyTrueText = StructLib[name].destroyTrueText;
-    this.attackFalseText = StructLib[name].attackFalseText;
-    this.attackTrueText = StructLib[name].attackTrueText;
-    this.speakFalseText = StructLib[name].speakFalseText;
-    this.speakTrueText = StructLib[name].speakTrueText;
-    this.activateTrueText = StructLib[name].activateTrueText;
-    this.activateFalseText = StructLib[name].activateFalseText;
-    this.deactivateTrueText = StructLib[name].deactivateTrueText;
-    this.deactivateFalseText = StructLib[name].deactivateFalseText;
+    this.moveFalseText = StructText[name].moveFalseText;
+    this.moveTrueText = StructText[name].moveTrueText;
+    this.takeFalseText = StructText[name].takeFalseText;
+    this.takeTrueText = StructText[name].takeTrueText;
+    this.destroyFalseText = StructText[name].destroyFalseText;
+    this.destroyTrueText = StructText[name].destroyTrueText;
+    this.attackFalseText = StructText[name].attackFalseText;
+    this.attackTrueText = StructText[name].attackTrueText;
+    this.speakFalseText = StructText[name].speakFalseText;
+    this.speakTrueText = StructText[name].speakTrueText;
+    this.activateTrueText = StructText[name].activateTrueText;
+    this.activateFalseText = StructText[name].activateFalseText;
+    this.deactivateTrueText = StructText[name].deactivateTrueText;
+    this.deactivateFalseText = StructText[name].deactivateFalseText;
 
-    this.inspectTextInactive = StructLib[name].inspectTextInactive;
-    this.inspectTextActive = StructLib[name].inspectTextActive;
+    this.inspectTextInactive = StructText[name].inspectTextInactive;
+    this.inspectTextActive = StructText[name].inspectTextActive;
 
-    // this.inspectTextInactive = StructLib[name].inspectTextInactive;
-    // this.inspectTextActive = StructLib[name].inspectTextActive;
-    // this.useTextUsed = StructLib[name].useTextUsed;
-    // this.useTextNotUsable = StructLib[name].useTextNotUsable;
+    // this.inspectTextInactive = StructText[name].inspectTextInactive;
+    // this.inspectTextActive = StructText[name].inspectTextActive;
+    // this.useTextUsed = StructText[name].useTextUsed;
+    // this.useTextNotUsable = StructText[name].useTextNotUsable;
     // this.useFunction = null;
     // this.inspectText = this.inspectTextInactive; // initialize inspectText to this
   }
 
   inspect() {
     if (this.activated) {
-      this.inspectText = this.inspectTextActive;
+      return this.inspectTextActive;
     }
     return this.inspectText;
   }
