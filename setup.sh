c34751528ef483fd38e5754ccc631edf0aa72dcb
Maria Volpe <mariaCHVolpe@gmail.com>
2019-05-12 01:28:42 -0400

    Update script and readme

--- setup.sh
+++ setup.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-git clone https://github.com/MariaVolpe/EscapeNLP.git
-cd EscapeNLP
 npm install
 cat ./nlp-classifier/nlp-classifier.js > ./node_modules/node-nlp/lib/nlp/nlp-classifier.js
 cd client
