6e3df60b12311fd64418b701d1bf30d655d4a3b8
Ishmaelk <kheir1@msn.com>
2019-05-15 12:08:36 -0400

    ran linter

--- game-logic/util.js
+++ game-logic/util.js
@@ -1,18 +1,14 @@
 
-const isCoordinate = (obj) => {
-  return !(obj.x === undefined && obj.y === undefined);
-}
+const isCoordinate = obj => !(obj.x === undefined && obj.y === undefined);
 
-const matchRegex = (regex, input) => {
-  return input.match(regex);
-}
+const matchRegex = (regex, input) => input.match(regex);
 
 const convertToIndices = (coordinate) => {
   const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   return {
-    x: Number.parseInt(coordinate.substring(1))-1, 
+    x: Number.parseInt(coordinate.substring(1)) - 1,
     y: alphabet.indexOf(coordinate[0]),
   };
-}
+};
 
 module.exports = { isCoordinate, matchRegex, convertToIndices };
