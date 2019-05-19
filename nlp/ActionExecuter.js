41384fe3c4b14f5802f0ab04f23d827c34eb1fc0
Maria Volpe <MariaCHVolpe@gmail.com>
2019-05-19 12:34:44 -0400

    Execute speak (#138)
    * Add speakable attribute to board
    
    * Add executeSpeak function
    
    * Foreach loop not c style for loop
    
    * Store move data properly into results on speak action

--- nlp/ActionExecuter.js
+++ nlp/ActionExecuter.js
@@ -327,7 +327,32 @@ class ActionExecuter {
   }
 
   executeSpeak(data) {
+    const user = this.grid.getObject({ identifier: data.userName });
+    const results = [];
+    const targets = data.directObjects; // these are direct objects, for some reason
 
+    targets.forEach((targetName) => {
+      const targetObj = this.grid.getObject({ searchOriginObj: user, identifier: targetName });
+      let text;
+
+      if (!targetObj) {
+        return results.push({ id: null, objectName: targetName, text: null, successful: false });
+      }
+
+      if (getDistance(user, targetObj) > 1) {
+        results.push(this.executeMove(data).result[0]);
+      }
+
+      if (!targetObj.speakable){
+        text = StructText[targetName] ? StructText[targetName].speakFalseText : null;
+        results.push({ id: null, objectName: targetName, text: text, successful: false, coordinates: null });
+      }
+
+      text = StructText[targetName] ? StructText[targetName].speakTrueText : null;
+      results.push({ id: targetObj.id, objectName: targetName, text: text, successful: true, coordinates: targetObj.position });
+    });
+
+    return { userName: user.name, action: 'speak', result: results };
   }
 
   executeActivate(data) {
